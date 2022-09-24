682a6cf48b5b7932b49d19997a16fb2e25f7c312
Mutuma Gitonga <mgitonga150@gmail.com>
2022-09-24 21:20:10 +0300

    Completed assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const[cartClass, setCartClass] = useState("");
+  // const[buttonText, setButtonText] = useState("Add to Cart");
+
+  function updateCart() { 
+    if(cartClass === "") {
+      setCartClass("in-cart");
+    } 
+      else if(cartClass === "in-cart") {
+        setCartClass(""); 
+      }
+  }
+ 
   return (
-    <li className="">
+    <li className={cartClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={() => updateCart()}>{cartClass === "" ? "Add to Cart" : "Remove From Cart"}</button>
     </li>
   );
 }
